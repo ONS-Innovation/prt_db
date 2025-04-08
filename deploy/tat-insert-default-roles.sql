492ce26d973ea60adf467806df5e69435b1a3d40
Kieran Pritchard <kieran.pritchard@ons.gov.uk>
2025-04-08 16:59:35 +0100

    Insert default user roles into tat.role

---
+++ deploy/tat-insert-default-roles.sql
@@ -0,0 +1,13 @@
+-- Deploy prt_db:tat-insert-default-roles to pg
+-- requires: tat-schema
+-- requires: tat-role
+
+BEGIN;
+
+INSERT INTO tat.role (name)
+VALUES 
+('Technical Contact'),
+('Delivery Manager'),
+('Editor');
+
+COMMIT;
