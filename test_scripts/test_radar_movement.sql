13111030256fd768c064854a5d35435dd0b41f85
Kieran Pritchard <kieran.pritchard@ons.gov.uk>
2025-04-11 13:58:20 +0100

    Add comments to test_radar_movement.sql

--- test_scripts/test_radar_movement.sql
+++ test_scripts/test_radar_movement.sql
@@ -1,3 +1,8 @@
+-- This script inserts example values into dbo.technology and digital_landscape.radar_timeline to simulate a technology being moved between rings.
+-- This is helpful to test that digital_landscape.ring_movement is working correctly.
+
+-- Note: If the database has pre-existing values, some of the ID's passed during insertion may need to be changed.
+
 INSERT INTO dbo.technology (name, quadrant_id, technology_type_id)
 VALUES ('Python', 2, 3);
 
